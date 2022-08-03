44ab83ee6f0a4730163a0a2645feef4323e0e07d
petprog <farinupeter@gmail.com>
2022-08-03 22:21:11 +0100

    add ArticleResponse.kt

--- core/src/main/java/com/demoapp/petprog/core/response/ArticleResponse.kt
+++ core/src/main/java/com/demoapp/petprog/core/response/ArticleResponse.kt
@@ -1,26 +1,23 @@
-/*
- * Created by Yogi Dewansyah
- * URL: https://github.com/yodeput
- * Copyright (c) 2022 . All rights reserved.
- * Last modified 2/23/22, 10:25 PM
- *
- */
-
-package com.yodeput.newsapp.core.response
+package com.demoapp.petprog.core.response
 
 
 import android.os.Parcelable
+import com.demoapp.petprog.core.model.News
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import com.yodeput.newsapp.core.model.News
 import kotlinx.parcelize.Parcelize
 
 @JsonClass(generateAdapter = true)
 @Parcelize
 data class ArticleResponse(
-    @Json(name = "articles") val articles: List<News>? = null,
-    @Json(name = "status") val status: String? = null,
-    @Json(name = "totalResults") val totalResults: Int? = null
+    @Json(name = "articles")
+    val articles: List<News>? = null,
+
+    @Json(name = "status")
+    val status: String? = null,
+
+    @Json(name = "totalResults")
+    val totalResults: Int? = null
 ) : Parcelable {
     companion object {
         fun mock() = ArticleResponse(
@@ -31,17 +28,6 @@ data class ArticleResponse(
                 News.mock(),
                 News.mock(),
                 News.mock(),
-                News.mock(),
-                News.mock(),
-                News.mock(),
-                News.mock(),
-            )
-        )
-
-        fun mockHeadline() = ArticleResponse(
-            status = "ok",
-            totalResults = 4,
-            articles = listOf(
                 News.mock(),
                 News.mock(),
                 News.mock(),
