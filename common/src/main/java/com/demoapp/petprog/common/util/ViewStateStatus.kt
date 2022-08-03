cdb58795b2055503579fc83195f618c38cee37ec
petprog <farinupeter@gmail.com>
2022-08-03 23:15:34 +0100

    add ViewStateStatus

--- common/src/main/java/com/demoapp/petprog/common/util/ViewStateStatus.kt
+++ common/src/main/java/com/demoapp/petprog/common/util/ViewStateStatus.kt
@@ -1,8 +1,8 @@
 package com.demoapp.petprog.common.util
 
-sealed class ViewState<out R> {
-    object Idle : ViewState<Nothing>()
-    object Loading : ViewState<Nothing>()
-    data class Success<out T>(val data: T) : ViewState<T>()
-    data class Error(val errorCode: ErrorCode?) : ViewState<Nothing>()
+sealed class ViewStateStatus<out R> {
+    object Idle : ViewStateStatus<Nothing>()
+    object Loading : ViewStateStatus<Nothing>()
+    data class Success<out T>(val data: T) : ViewStateStatus<T>()
+    data class Error(val errorCode: ErrorCode?) : ViewStateStatus<Nothing>()
 }
