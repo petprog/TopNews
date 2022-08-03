3d5b8f1eea329c27f7049a14953d42770f50c88f
petprog <farinupeter@gmail.com>
2022-08-03 23:25:34 +0100

    add getHeadline() to emit articles the result is success and also save it to local database

--- core/src/main/java/com/demoapp/petprog/core/repository/NewsRepository.kt
+++ core/src/main/java/com/demoapp/petprog/core/repository/NewsRepository.kt
@@ -1,8 +1,18 @@
 package com.demoapp.petprog.core.repository
 
+import ErrorResponseMapper
 import com.demoapp.petprog.common.base.RepositoryI
+import com.demoapp.petprog.common.util.ErrorCode
+import com.demoapp.petprog.common.util.ViewStateStatus
 import com.demoapp.petprog.core.dao.NewsDao
+import com.demoapp.petprog.core.database.NewsDb
+import com.demoapp.petprog.core.model.News
 import com.demoapp.petprog.core.network.NewsService
+import com.skydoves.sandwich.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -16,5 +26,48 @@ class NewsRepository @Inject constructor(
     init {
         Timber.d("Injection NewsRepository")
     }
+
+    fun fetchHeadline(): Flow<ViewStateStatus<List<News>>> = flow {
+        val result = api.headline()
+        result.suspendOnSuccess {
+            data.articles!!.let {
+                emit(ViewStateStatus.Success(it))
+                dao.insert(
+                    NewsDb(
+                        id = 1,
+                        description = "headline",
+                        data = it
+                    )
+                )
+            }
+        }.suspendOnError {
+            val error = map(ErrorResponseMapper)
+            emit(ViewStateStatus.Error(error))
+        }.suspendOnException {
+            emit(ViewStateStatus.Error(ErrorCode(1000, message())))
+        }
+    }.flowOn(Dispatchers.IO)
+
+    fun getHeadline(
+        isRefresh: Boolean
+    ): Flow<ViewStateStatus<List<News>>> = flow {
+        emit(ViewStateStatus.Loading)
+        dao.getHeadline().let { dataDb ->
+            if (dataDb != null) {
+                if (isRefresh) {
+                    fetchHeadline().collect { fetch ->
+                        emit(fetch)
+                    }
+                } else {
+                    emit(ViewStateStatus.Success(dataDb.data))
+                }
+            }  else {
+                fetchHeadline().collect { fetch ->
+                    emit(fetch)
+                }
+            }
+        }
+    }.flowOn(Dispatchers.IO)
+
 
 }
